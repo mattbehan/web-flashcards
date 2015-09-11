db5afac159843df049b3e6ac762b930e1d041fdd
David Campbell <davidcampbell.itprofessional@gmail.com>
2015-09-11 18:22:17 -0500

    first pass

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+require 'bcrypt'
+class User < ActiveRecord::Base
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def correct_password?(pwd)
+    self.password == pwd
+  end
+
+end
